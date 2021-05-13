2a46e201e7c7afee05d10a447399cb29cc19a944
Mateusz Gil <mateusz.gil@oktawave.com>
2021-05-13 16:31:52 +0200

    github-actioins

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-
-set -e
+set -eo pipefail
 
 CMD="${INPUT_RUN/$'\n'/' && '}"
 
-echo "INPUT_HOST: ${INPUT_HOST}"
-echo "INPUT_PORT": ${INPUT_PORT}
+function main() {
+    configSSHAccessKey
+
+  if [ "${INPUT_ACTION}" == "scp" ]; then
+    scp
+  elif [ "${INPUT_ACTION}" == "scp" ]; then
+    ssh-command
+  fi
+}
+
+function configSSHAccessKey() {
+  mkdir "~/.ssh"
+  echo "$INPUT_KEY" > "/root/.ssh/id_rsa"
+  chmod 0400 "~/.ssh/id_rsa"
+}
+
+function ssh-command() {
+  ssh -v -o StrictHostKeyChecking=no -p "$INPUT_PORT" "$INPUT_USER"@"$INPUT_HOST" "$CMD"
+}
+
+main "$@"
